7b9756308906ff77e53d0bca5f156252de76f63c
Graham Bruce <grahambruce84@gmail.com>
2017-08-20 21:11:22 +0100

    working through the integration tests

--- js_calculator_start_point/tests/integration/tests.js
+++ js_calculator_start_point/tests/integration/tests.js
@@ -7,14 +7,101 @@ describe('calculator functionality', function() {
   beforeEach(function() {
     browser.ignoreSynchronization = true;
     browser.get('http://localhost:3000');
+    running_total = element(by.css('#running_total'))
+    two = element(by.css('#number2'));
+    three = element(by.css('#number3'));
+    four = element(by.css('#number4'));
+    eight = element(by.css('#number8'));
+    add = element(by.css('#operator_add'));
+    subtract = element(by.css('#operator_subtract'));
+    divide = element(by.css('#operator_divide'));
+    multiply = element(by.css('#operator_multiply'));
+    equals = element(by.css('#operator_equals'));
   });
 
   // write integration tests here in the form of "it should do something..."
  it('should have working number buttons', function(){
-    running_total = element(by.css('#running_total'))
     element(by.css('#number1')).click();
     element(by.css('#number9')).click();
     expect(running_total.getAttribute('value')).to.eventually.equal('19')
  });
 
+ it('should add 2 and 4 to make 6', function(){
+    two.click();
+    add.click();
+    four.click();
+    equals.click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('6')
+ })
+
+ it('should subtract 2 from 4 to make 2', function(){
+  four.click();
+  subtract.click();
+  two.click();
+  equals.click();
+  expect(running_total.getAttribute('value')).to.eventually.equal('2')
+ })
+
+ it('should divide 4 by 2 to give 2', function(){
+  four.click();
+  divide.click();
+  two.click();
+  equals.click();
+  expect(running_total.getAttribute('value')).to.eventually.equal('2')
+ })
+
+ it('should multiply 4 by 2 to give 8', function(){
+  four.click();
+  multiply.click();
+  two.click();
+  equals.click();
+  expect(running_total.getAttribute('value')).to.eventually.equal('8')
+ })
+
+ it('should chain multiple operations together', function(){
+  four.click();
+  two.click();
+  add.click();
+  expect(running_total.getAttribute('value')).to.eventually.equal('42')
+  three.click();
+  subtract.click();
+  expect(running_total.getAttribute('value')).to.eventually.equal('45')
+  three.click();
+  equals.click();
+  expect(running_total.getAttribute('value')).to.eventually.equal('42')
+ })
+
+ it('should work with a negative numbers', function(){
+  four.click();
+  add.click();
+  two.click();
+  subtract.click();
+  eight.click();
+  equals.click();
+  expect(running_total.getAttribute('value')).to.eventually.equal('-2')  
+ })
+
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
